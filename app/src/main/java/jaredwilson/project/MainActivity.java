762ca1bfe1bfbd787dca575ce75ad3ac5554e710
Jared The Two-Handed Wilson <j.to.the.ared@gmail.com>
2016-02-26 20:21:37 -0700

    More complex

--- app/src/main/java/jaredwilson/project/MainActivity.java
+++ app/src/main/java/jaredwilson/project/MainActivity.java
@@ -7,14 +7,17 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 0;
     }
 
     public void onClick (View v) {
+        counter++;
     Button button = (Button) findViewById(R.id.button);
-        button.setText("Change");
+        button.setText("" + counter);
     }
 }
