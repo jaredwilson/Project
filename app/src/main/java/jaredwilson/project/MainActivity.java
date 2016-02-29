7ed7d60f1a9ad47deddaeb5e9adbac2efa9f4c85
Andy Barton <bar14050@byui.edu>
2016-02-29 10:01:55 -0700

    Andy's changes with more buttons!!

--- app/src/main/java/jaredwilson/project/MainActivity.java
+++ app/src/main/java/jaredwilson/project/MainActivity.java
@@ -8,11 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
-    MediaRecorder rRecorder;
+    MediaRecorder mRecorder;
+    MediaPlayer mPlayer;
     MediaPlayer jamsesh;
+    File fileName;
     public boolean playpress;
+    public boolean isRecording;
+    public boolean isPlaying;
     int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         counter = 0;
         playpress = true;
+        isRecording = false;
         jamsesh = MediaPlayer.create(this, R.raw.jammin);
-
+        fileName = new File(this.getFilesDir(), "Most Recent");
 
         try {
             jamsesh.prepare();
@@ -65,5 +73,61 @@ public class MainActivity extends AppCompatActivity {
         }*/
     }
 
+    public void record_actions(View view) {
+        Button mRecorder = (Button)view;
 
+        if(isRecording) {
+            startRecording();
+            mRecorder.setText("Stop Recording");
+        } else {
+            stopRecording();
+            mRecorder.setText("Start Recording");
+        }
+        isRecording = !isRecording;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(fileName.getPath());
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) { e.printStackTrace(); }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    public void play_actions(View view) {
+        Button mPlayer = (Button)view;
+
+        if(isPlaying) {
+            startPlaying();
+            mPlayer.setText("Stop Playing");
+        } else {
+            stopPlaying();
+            mPlayer.setText("Start Playing");
+        }
+        isPlaying = !isPlaying;
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(fileName.getPath());
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (Exception e){ e.printStackTrace(); }
+    }
+    private void stopPlaying() {
+
+    }
 }
