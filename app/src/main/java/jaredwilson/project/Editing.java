2cdd5d0ae869e817de2f717a7211bb9f4b0ff6c7
Nathan Ulmer <nathanulmer@gmail.com>
2016-03-31 10:58:36 -0600

    Editing (Megaphone) fix

--- app/src/main/java/jaredwilson/project/Editing.java
+++ app/src/main/java/jaredwilson/project/Editing.java
@@ -1,27 +1,11 @@
 package jaredwilson.project;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import java.io.File;
-import java.io.IOException;
 
 public class Editing extends AppCompatActivity {
     public final static String key = "key";
